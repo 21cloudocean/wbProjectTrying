25a78d4c03571b9da1d82ee994d78e7076b2d3d9
21cloudocean <437508630@qq.com>
2024-07-08 09:59:50 +0800

    JS首页关注icon toggle+发现页nav和footer加链接

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello!')
+// elements
+const drop = document.querySelector('.drop')
+const dropIcon = document.querySelector('.drop-icon')
+// navbar toggle
+// 啊？切换的页面里没有这个element会报错啊 先加上try/catch了 不知道有没有别的更好的办法
+try {
+  drop.addEventListener('click', function (e) {
+    if (drop.classList.contains('selected')) {
+      e.preventDefault()
+      dropIcon.classList.toggle('rotate')
+      console.log(e.currentTarget)
+    } else return
+  })
+} catch {}
+
+// functions
