488017bbfaa0007ae52c957d76c674fff5af1af2
21cloudocean <437508630@qq.com>
2024-07-14 12:36:59 +0800

    个人主页头像透明度+筛选栏布局+每条内容布局

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // elements
 const drop = document.querySelector('.drop')
 const dropIcon = document.querySelector('.drop-icon')
+const selfPageImg = document.querySelector('.selfpage-title-img-container')
+const contentContainer = document.querySelector('.usericon')
+const selfpageNav = document.querySelector('.self-page-nav')
+// 用到的数值
+const selfpageNavBottom = selfpageNav.getBoundingClientRect().bottom
+// const fixedElementTop = contentContainer.getBoundingClientRect().top
+const fixedElementBottom = selfPageImg.getBoundingClientRect().bottom
 // navbar toggle
 // 啊？切换的页面里没有这个element会报错啊 先加上try/catch了 不知道有没有别的更好的办法
 try {
@@ -12,5 +19,18 @@ try {
     } else return
   })
 } catch {}
+// 个人主页向上滚动头像变透明
+window.addEventListener('scroll', function () {
+  // 计算元素的底部距离顶部的距离
+  // const newElementTop = contentContainer.getBoundingClientRect().top
+  const newElementBottom = selfPageImg.getBoundingClientRect().bottom
+  const changingDistance = newElementBottom - selfpageNavBottom
+  const fixedDistance = fixedElementBottom - selfpageNavBottom
+  // 设置透明度，距离顶部越远，透明度越高
+  const newOpacity = Math.min(1, changingDistance / fixedDistance)
+  selfPageImg.style.opacity = newOpacity
+  // console.log('elementbottom:' + elementBottom)
 
+  console.log(newOpacity)
+})
 // functions
