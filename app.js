d5ef15f57442378c782e2fd7b2c573b51a45832d
21cloudocean <437508630@qq.com>
2024-07-16 00:30:50 +0800

    下拉菜单加banner+JS点击关闭下拉菜单/下拉icon旋转

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 // elements
 const drop = document.querySelector('.drop')
 const dropIcon = document.querySelector('.drop-icon')
-
+const menuBanner = document.querySelector('.menu-banner')
+const menuList = [...document.querySelectorAll('.menu-list-line')]
+console.log(menuList)
 // navbar toggle
 // 啊？切换的页面里没有这个element会报错啊 先加上try/catch了 不知道有没有别的更好的办法
 try {
   drop.addEventListener('click', function (e) {
     if (drop.classList.contains('selected')) {
       e.preventDefault()
-      dropIcon.classList.toggle('rotate')
-      console.log(e.currentTarget)
+      dropIcon.classList.add('rotate')
+      // console.log(e.currentTarget)
+      menuBanner.classList.remove('none')
     } else return
+  })
+  menuBanner.addEventListener('click', function (e) {
+    if (e.target === menuBanner) {
+      // console.log(e.target)
+      dropIcon.classList.remove('rotate')
+      menuBanner.classList.add('none')
+    }
+    // else {
+    //   e.target.classList.add('selected')
+    // }
   })
 } catch {}
 
