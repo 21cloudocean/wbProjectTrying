3ad5ca1ba63e8f647c3e97480311b521e0b408b7
21cloudocean <437508630@qq.com>
2024-07-17 10:47:32 +0800

    JS：首页关注下拉菜单选中某项效果

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const dropIcon = document.querySelector('.drop-icon')
 const menuBanner = document.querySelector('.menu-banner')
 const menuList = [...document.querySelectorAll('.menu-list-line')]
 console.log(menuList)
-// navbar toggle
+// index页 navbar toggle
 // 啊？切换的页面里没有这个element会报错啊 先加上try/catch了 不知道有没有别的更好的办法
 try {
   drop.addEventListener('click', function (e) {
@@ -15,16 +15,21 @@ try {
       menuBanner.classList.remove('none')
     } else return
   })
+  menuList.forEach((menuItem) => {
+    menuItem.addEventListener('click', function (e) {
+      menuList.forEach(function (item) {
+        item.classList.remove('selected')
+        // console.log('remove')
+      })
+      e.currentTarget.classList.add('selected')
+    })
+  })
   menuBanner.addEventListener('click', function (e) {
     if (e.target === menuBanner) {
       // console.log(e.target)
       dropIcon.classList.remove('rotate')
       menuBanner.classList.add('none')
     }
-    // else {
-    //   e.target.classList.add('selected')
-    // }
   })
 } catch {}
-
 // functions
