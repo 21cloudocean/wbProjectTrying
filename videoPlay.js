ac2ae2b879fc0946d58864f772fe2e078f8abd80
21cloudocean <437508630@qq.com>
2024-07-22 23:34:34 +0800

    JS：视频页进度条显示视频播放进度；点击进度条实现跳转；视频结束后显示播放按钮

--- videoPlay.js
+++ videoPlay.js
@@ -1,25 +1,40 @@
 window.onload = function () {
   // elements
   const video = document.querySelector('video')
-  // const videoPlayIcon = document.querySelector('.video-play-icon')
-  const progreeeIcon = document.querySelector('.progress-icon')
   const videoPlayControl = document.querySelector('.videopage-playcontrol')
+  // const videoPlayIcon = document.querySelector('.video-play-icon')
+  const wholeBar = document.querySelector('.progress-bar-parent')
+  const currentBar = document.querySelector('.progress-bar')
+  const progressIcon = document.querySelector('.progress-icon')
 
   // 进度条所需时间
-  let timePlayed = video.currentTime
-  const timeTotal = video.duration
-  console.log(timePlayed, timeTotal)
-  let flag = true
+  let timer = setInterval(function () {
+    let timePlayed = video.currentTime
+    const timeTotal = video.duration
+    let progress = (timePlayed / timeTotal) * 100 + '%'
+    currentBar.style.width = progress
+    progressIcon.style.left = progress
+  }, 100)
+  // 进度条拖动进度
+  wholeBar.addEventListener('click', function (e) {
+    currentBar.style.width = e.offsetX + 'px'
+    progressIcon.style.left = e.offsetX + 'px'
+    video.currentTime = (e.offsetX / wholeBar.clientWidth) * video.duration
+    video.play()
+  })
+
   // 点击视频后视频暂停
   video.addEventListener('click', function () {
-    flag = false
     videoPlayControl.classList.remove('none')
     video.pause()
   })
   // 点击播放按钮后视频播放
   videoPlayControl.addEventListener('click', function () {
-    flag = true
     videoPlayControl.classList.add('none')
     video.play()
+  })
+  // 视频结束后显示播放按钮
+  video.addEventListener('ended', function () {
+    videoPlayControl.classList.remove('none')
   })
 }
